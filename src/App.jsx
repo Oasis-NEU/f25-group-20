aa34bb96436452af0486e4f17c4a27d2fa3637e3
Arina Babikyan <babikyan.a@northeastern.edu>
2025-10-12 13:50:39 -0400

    improved visual style, work for hack4 session done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div style={{ width: '100vw',      // full width of the window
     height: '100vh',     // full height of the window
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#496068',
     padding: '20px'}}>
       {/* Top bar */}
       <div 
@@ -16,17 +16,17 @@ const App = () => {
           marginBottom: '20px'
         }}
       >
-        <h1 style={{display: 'flex'}}>Welcome back, "Name"!</h1>
+        <h1 style={{color: 'white', display: 'flex'}}>Welcome back, "Name"!</h1>
         <button style = {{marginRight: "70px", marginTop: "40px", padding: "20px"}}>Create new document</button>
       </div>
 
-      {/* Main content */}
-      <div style={{ display: 'flex', gap: '20px' }}>
-        <div style={{ flex: 2, backgroundColor: '#f0f0f0', padding: '20px' }}>
-          Left content (2/3 width)
+      {/* Tabs */}
+      <div style={{ display: 'flex', gap: '20px', height: '70vh'}}>
+        <div style={{ flex: 2, backgroundColor: '#f0f0f0', padding: '20px'}}>
+         View documents
         </div>
         <div style={{ flex: 1, backgroundColor: '#d0d0d0', padding: '20px' }}>
-          Right content (1/3 width)
+          View upcoming assignments
         </div>
       </div>
     </div>
